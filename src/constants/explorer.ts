96bcec5938cbd8b178017ef4300b830d1743fec2
Zack Balbin <zack@zackbalbin.com>
2022-09-04 13:32:28 -0500

    change testnet explorer urls

--- src/constants/explorer.ts
+++ src/constants/explorer.ts
@@ -2,10 +2,10 @@ import { MAINNET } from "./ergo";
 
 export const API_URL = MAINNET
   ? "https://api.ergoplatform.com"
-  : "https://api-testnet.ergoplatform.com";
+  : "http://69.164.215.107:8080";
 export const ADDRESS_URL = MAINNET
   ? "https://explorer.ergoplatform.com/en/addresses/"
-  : "https://testnet.ergoplatform.com/en/addresses/";
+  : "http://69.164.215.107:3000/en/addresses/";
 export const TRANSACTION_URL = MAINNET
   ? "https://explorer.ergoplatform.com/en/transactions/"
-  : "https://testnet.ergoplatform.com/en/transactions/";
+  : "http://69.164.215.107:3000/en/transactions/";
